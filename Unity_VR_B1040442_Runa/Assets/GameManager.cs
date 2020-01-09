1eaa3b4857c45069ecbc85fa3b24e1b1c8c06198
chloe0936210817 <55518289+chloe0936210817@users.noreply.github.com>
2020-01-09 14:03:58 +0800

    7777

---
+++ Unity_VR_B1040442_Runa/Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool haveKey;
+
+    public void ThrowPickUp()
+    {
+        print("檢起物件");
+    }
+
+    public void ThrowDetach(GameObject obj)
+    {
+        print("放起物件");
+        haveKey = true;
+        Destroy(obj);
+    }
+
+    public void ThrowHeld()
+    {
+        print("拿起物件");
+    }
+}
---
+++ Unity_VR_B1040442_Runa/Assets/TeleportInteratble.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportInteratble : MonoBehaviour
+{
+    [Header("動畫控制器")]
+    public Animator animatorControl;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "HeadCollider" && GameManager.haveKey)
+        {
+            animatorControl.SetBool("isOpen_Obj_1",true);
+        }
+    }
+}
